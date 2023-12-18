937e96b26bda8c2f858c9a0f0d66c6b40c3f4d14
raphaellutz <lutzraphael8@gmail.com>
2023-12-18 22:31:56 +0100

    Endpoint angepasst

--- src/main/java/com/testForAwsDeployment/testDeployment/TestController.java
+++ src/main/java/com/testForAwsDeployment/testDeployment/TestController.java
@@ -1,5 +1,6 @@
 package com.testForAwsDeployment.testDeployment;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class TestController {
 
-    @GetMapping("/test")
-    public ResponseEntity<?> testEndpoint() {
-        return ResponseEntity.ok("Successful");
+    @GetMapping(value = "/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getTest() {
+        String responseBody = "test";
+        return ResponseEntity.ok().body(responseBody);
     }
 }
 
