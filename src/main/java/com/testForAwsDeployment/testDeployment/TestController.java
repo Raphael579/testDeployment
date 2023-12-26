344b97983e052236ce21f9a66d89173070158433
raphaellutz <lutzraphael8@gmail.com>
2023-12-26 17:38:59 +0100

    Endpoint angepasst

--- src/main/java/com/testForAwsDeployment/testDeployment/TestController.java
+++ src/main/java/com/testForAwsDeployment/testDeployment/TestController.java
@@ -1,30 +1,20 @@
 package com.testForAwsDeployment.testDeployment;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
 public class TestController {
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/info")
-    public Map<String, Object> getInfo() {
-        Map<String, Object> responseBody = new HashMap<>();
-
-        responseBody.put("isBase64Encoded", false);
-        responseBody.put("statusCode", 200);
-        responseBody.put("statusDescription", "200 OK");
-
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        responseBody.put("headers", headers);
-
-        responseBody.put("body", "Hallo Welt");
-        return responseBody;
+    public ResponseEntity<String> getInfo() {
+        return ResponseEntity.ok("Successful");
     }
 }
 
